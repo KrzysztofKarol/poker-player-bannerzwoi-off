572f34ef4ae61fb2cba12b1abbe0185f392a0232
Jan Nicklas <j.nicklas@me.com>
2022-01-26 10:40:34 +0100

    remove typings

--- src/Player.test.ts
+++ src/Player.test.ts
@@ -1,7 +1,7 @@
 import Player from "./Player";
 
 describe("Player", () => {
-    it("bets", async () => {
+    it("bets all in for > 50%", async () => {
         const player = new Player();
         expect(await player.betRequest({
             "players":[
@@ -19,7 +19,56 @@ describe("Player", () => {
                 "stack":1000,
                 "status":"active",
                 "bet":0,
+                "hole_cards":[{
+                    suit: "clubs",
+                    rank: "A"
+                },{
+                    suit: "clubs",
+                    rank: "K"
+                }],
+                "version":"Version name 2",
+                "id":1
+              }
+            ],
+            "tournament_id":"550d1d68cd7bd10003000003",
+            "game_id":"550da1cb2d909006e90004b1",
+            "round":0,
+            "bet_index":0,
+            "small_blind":10,
+            "orbits":0,
+            "dealer":0,
+            "community_cards":[],
+            "current_buy_in":0,
+            "pot":0
+          })).toBe(10000)
+    });
+
+
+    it("bets all in for > 50%", async () => {
+        const player = new Player();
+        expect(await player.betRequest({
+            "players":[
+              {
+                "name":"Player 1",
+                "stack":1000,
+                "status":"active",
+                "bet":0,
                 "hole_cards":[],
+                "version":"Version name 1",
+                "id":0
+              },
+              {
+                "name":"Player 2",
+                "stack":1000,
+                "status":"active",
+                "bet":0,
+                "hole_cards":[{
+                    suit: "clubs",
+                    rank: "2"
+                },{
+                    suit: "clubs",
+                    rank: "3"
+                }],
                 "version":"Version name 2",
                 "id":1
               }
@@ -38,7 +87,7 @@ describe("Player", () => {
     })
 
 
-    it("bets", () => {
+    it("getStartingHandScore", () => {
         const player = new Player();
         expect(player.getStartingHandScore([{
             suit: "clubs",
